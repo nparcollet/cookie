db5f03fb78b23a02d5754c837f1097b866b868d1
Nicolas Parcollet <parcollet@gmail.com>
2018-10-14 21:59:21 +0200

    glib-2.58.1: added package

---
+++ packages/community/glib/glib-2.58.1.mk
@@ -0,0 +1,30 @@
+P_NAME			= glib
+P_VERSION		= 2.58.1
+P_DESCRIPTION	= Core application building blocks for libraries and applications written in C
+P_ARCHIVE		= glib-2.58.1.tar.xz
+P_URL			= https://download.gnome.org/sources/glib/2.58/$(P_ARCHIVE)
+P_LICENCES		= LGPL
+P_ARCHS			= arm
+P_DEPENDS		= zlib libffi libpcre
+P_SRCDIR		= glib-2.58.1
+
+.PHONY: fetch setup compile install
+
+fetch:
+	cookie fetch $(P_URL) $(P_ARCHIVE)
+
+setup:
+	cookie extract $(P_ARCHIVE) $(P_WORKDIR)
+	# TODO: a cookie patch command that take automatically patch if a patch exists
+	cd $(P_SRCDIR) && patch -p1 < $(P_PATCH)
+
+compile:
+	wget https://raw.githubusercontent.com/GNOME/gtk-doc-stub/master/gtk-doc.m4 -O gtk-doc.m4
+	wget https://raw.githubusercontent.com/GNOME/gtk-doc-stub/master/gtk-doc.make -O gtk-doc.make
+	autoreconf -if
+	./configure --prefix=/usr --with-pcre=system --sysconfdir=/etc --host=$(HOST) --enable-libmount=no glib_cv_stack_grows=no glib_cv_uscore=no
+	make -j$(P_NPROCS)
+
+install:
+	make DESTDIR=$(P_DESTDIR) install-strip
+	fix-la-files
