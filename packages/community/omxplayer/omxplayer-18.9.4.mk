cb2b2ff3d6b83fe069add180e2016c4f07e68405
Nicolas Parcollet <parcollet@gmail.com>
2018-10-16 13:52:54 +0200

    omxplayer-18.9.4: fixed package

--- packages/community/omxplayer/omxplayer-18.9.4.mk
+++ packages/community/omxplayer/omxplayer-18.9.4.mk
@@ -6,7 +6,19 @@ P_GITREV		= 7f3faf6cadac913013248de759462bcff92f0102
 P_LICENCES		= GPL2
 P_ARCHS			= arm
 P_SRCDIR		= omxplayer
-P_DEPENDS		= alsa-lib
+P_DEPENDS		= alsa-lib pcre ffmpeg zlib dbus freetype
+
+FT_CFLAGS     = $(shell $(HOST)-pkg-config --cflags freetype2)
+DBUS_CFLAGS   = $(shell $(HOST)-pkg-config --cflags dbus-1)
+ALSA_CFLAGS   = $(shell $(HOST)-pkg-config --cflags alsa)
+PCRE_CFLAGS   = $(shell $(HOST)-pkg-config --cflags libpcre)
+ALL_CFLAGS	  = $(FT_CFLAGS) $(DBUS_CFLAGS) $(PCRE_CFLAGS) $(ALSA_CFLAGS)
+
+FT_LIBS     = $(shell $(HOST)-pkg-config --libs freetype2)
+DBUS_LIBS   = $(shell $(HOST)-pkg-config --libs dbus-1)
+ALSA_LIBS   = $(shell $(HOST)-pkg-config --libs alsa)
+PCRE_LIBS   = $(shell $(HOST)-pkg-config --libs libpcre)
+ALL_LIBS	  = $(FT_LIBS) $(DBUS_LIBS) $(PCRE_LIBS) $(ALSA_LIBS)
 
 fetch:
 	cookie git clone $(P_GITURL) $(P_NAME)
@@ -15,8 +27,11 @@ setup:
 	cookie git checkout $(P_NAME) $(P_GITREV) $(P_SRCDIR)
 
 compile:
-	HOST=$(HOST) FLOAT=$(ARM_FLOATABI) SDKSTAGE=$(P_SYSROOT) LDFLAGS="$(LDFLAGS)" INCLUDES="-isystem$(P_SYSROOT)/usr/include" make ffmpeg
-	# SDKSTAGE=$(P_SYSROOT) STRIP=$(HOST)-strip
-	#CC=$(HOST)-cc SDKSTAGE=$(P_SYSROOT) STRIP=$(HOST)-strip make
+	sed -e "s%CFLAGS=%CFLAGS+= $(ALL_CFLAGS) %" -i Makefile
+	sed -e "s%LDFLAGS=%LDFLAGS+= $(ALL_LIBS) %" -i Makefile
+	CC=$(HOST)-cc SDKSTAGE=$(P_SYSROOT) STRIP=$(HOST)-strip make -j$(P_NPROCS) omxplayer.bin
 
 install:
+	mkdir -p $(P_DESTDIR)/usr/bin
+	cp omxplayer $(P_DESTDIR)/usr/bin/
+	cp omxplayer.bin $(P_DESTDIR)/usr/bin/
