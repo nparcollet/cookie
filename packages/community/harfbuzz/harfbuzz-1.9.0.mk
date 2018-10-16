29fcc3efa1c5b8ce3ab367a69672df780ce989d5
Nicolas Parcollet <parcollet@gmail.com>
2018-10-16 13:52:17 +0200

    harfbuzz-1.9.0: cleanup package

--- packages/community/harfbuzz/harfbuzz-1.9.0.mk
+++ packages/community/harfbuzz/harfbuzz-1.9.0.mk
@@ -5,23 +5,24 @@ P_ARCHIVE		= harfbuzz-1.9.0.tar.bz2
 P_URL			= https://github.com/harfbuzz/harfbuzz/releases/download/1.9.0/$(P_ARCHIVE)
 P_LICENCES		= MIT
 P_ARCHS			= arm
-P_DEPENDS		=
+P_DEPENDS		= glib zlib bzip2 libpng freetype
 P_SRCDIR		= harfbuzz-1.9.0
 
 .PHONY: fetch setup compile install
 
 fetch:
 	cookie fetch $(P_URL) $(P_ARCHIVE)
+	cookie fetch https://download.savannah.gnu.org/releases/freetype/freetype-2.9.1.tar.bz2
 
 setup:
 	cookie extract $(P_ARCHIVE) $(P_WORKDIR)
+	cookie extract freetype-2.9.1.tar.bz2 $(P_WORKDIR)
 
 compile:
-	./configure --host=$(HOST) --prefix=/usr --without-freetype --without-glib
+	./configure --host=$(HOST) --prefix=/usr
 	make -j$(P_NPROCS)
 
 install:
 	make DESTDIR=$(P_DESTDIR) install
 	rm -rf $(P_DESTDIR)/usr/share
-	rm -rf $(P_DESTDIR)/usr/lib/cmake
 	fix-la-files $(P_DESTDIR)
