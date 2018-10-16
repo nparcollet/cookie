4629399fe9b00f0bda282962945349ee1455f491
Nicolas Parcollet <parcollet@gmail.com>
2018-10-16 13:49:03 +0200

    expat-2.2.6: added package

---
+++ packages/community/expat/expat-2.2.6.mk
@@ -0,0 +1,26 @@
+P_NAME			= expat
+P_VERSION		= 2.2.6
+P_DESCRIPTION	= Fast streaming XML parser written in C
+P_ARCHIVE		= expat-2.2.6.tar.bz2
+P_URL			= https://github.com/libexpat/libexpat/releases/download/R_2_2_6/$(P_ARCHIVE)
+P_LICENCES		= MIT
+P_ARCHS			= arm
+P_DEPENDS		=
+P_SRCDIR		= expat-2.2.6
+
+.PHONY: fetch setup compile install
+
+fetch:
+	cookie fetch $(P_URL) $(P_ARCHIVE)
+
+setup:
+	cookie extract $(P_ARCHIVE) $(P_WORKDIR)
+
+compile:
+	./configure --prefix=/usr --host=$(HOST)
+	make -j$(P_NPROCS)
+
+install:
+	make DESTDIR=$(P_DESTDIR) install
+	rm -rf $(P_DESTDIR)/usr/share
+	fix-la-files $(P_DESTDIR)
