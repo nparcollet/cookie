07c73635967fd79bd06d920548f32e2353aa361a
Nicolas Parcollet <parcollet@gmail.com>
2018-10-20 10:35:36 +0200

    sdl2-2.0.8: using a separate package name to have both sdl and sdl2 at the same time

--- packages/community/sdl2/sdl2-2.0.8.mk
+++ packages/community/sdl2/sdl2-2.0.8.mk
@@ -1,11 +1,11 @@
-P_NAME			= sdl
+P_NAME			= sdl2
 P_VERSION		= 2.0.8
-P_DESCRIPTION	= libpng is the official PNG reference library.
+P_DESCRIPTION	= Framework fpr low level access to audio, keyboard, mouse, joystick, and graphics
 P_ARCHIVE		= SDL2-2.0.8.tar.gz
 P_URL			= https://www.libsdl.org/release/$(P_ARCHIVE)
-P_LICENCES		=
+P_LICENCES		= ZLIB
 P_ARCHS			= arm
-P_DEPENDS		= alsa-lib glib
+P_DEPENDS		= alsa-lib glib firmware
 P_SRCDIR		= SDL2-2.0.8
 
 .PHONY: fetch setup compile install
@@ -13,13 +13,14 @@ P_SRCDIR		= SDL2-2.0.8
 fetch:
 	cookie fetch $(P_URL)
 
-
 setup:
 	cookie extract $(P_ARCHIVE) $(P_WORKDIR)
 
 compile:
-	./configure --host=$(HOST) --prefix=/usr
+	./autogen.sh
+	./configure --host=$(HOST) --prefix=/usr --enable-video-rpi
 	make -j$(P_NPROCS)
 
 install:
 	make DESTDIR=$(P_DESTDIR) install
+	rm -rf $(P_DESTDIR)/usr/bin
