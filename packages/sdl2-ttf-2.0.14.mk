3bf9477db91b61408cb9909844124290b8efe4e9
Nicolas Parcollet <parcollet@gmail.com>
2018-10-23 19:34:50 +0200

    sdl2-ttf-2.0.14: fixed name and description

--- packages/sdl2-ttf-2.0.14.mk
+++ packages/sdl2-ttf-2.0.14.mk
@@ -1,6 +1,6 @@
-P_NAME			= sdl2
+P_NAME			= sdl2-ttf
 P_VERSION		= 2.0.14
-P_DESCRIPTION	= TTF Support Library for STL2
+P_DESCRIPTION	= TTF Support Library for SDL2
 P_ARCHIVE		= SDL2_ttf-2.0.14.tar.gz
 P_URL			= https://www.libsdl.org/projects/SDL_ttf/release/$(P_ARCHIVE)
 P_LICENCES		= ZLIB
