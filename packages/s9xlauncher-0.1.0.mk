1d3ca416144556b02f01dfe820a7169ef6b32ee8
Nicolas Parcollet <parcollet@gmail.com>
2018-10-28 17:55:46 +0100

    s9xlauncher: added init script

--- packages/s9xlauncher-0.1.0.mk
+++ packages/s9xlauncher-0.1.0.mk
@@ -2,7 +2,7 @@ P_NAME			= s9xlauncher
 P_VERSION		= 0.1.0
 P_DESCRIPTION	= SNes Emulator
 P_GITURL		= https://github.com/nparcollet/s9xlauncher.git
-P_GITREV		= b6ca61e38a79247963b024d25309a20d7b0208c2
+P_GITREV		= 4a98a2a62b1c7f0314eadf25cf5173bf2a0072ca
 P_LICENCES		= MIT
 P_ARCHS			= arm
 P_DEPENDS		= sdl2 sdl2-ttf
@@ -24,4 +24,6 @@ compile:
 
 
 install:
+	mkdir -p $(P_DESTDIR)/etc/init.d
+	cp -a init.d/s900-s9xlauncher $(P_DESTDIR)/etc/init.d
 	cd build && make DESTDIR=$(P_DESTDIR) install-strip
