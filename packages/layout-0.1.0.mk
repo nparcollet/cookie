bbaa44ad029352f9286d3611bcd073aa3e4d0ce6
Nicolas Parcollet <parcollet@gmail.com>
2018-10-28 16:41:46 +0100

    layout-0.1.0: now use layout.tgz from profile

--- packages/layout-0.1.0.mk
+++ packages/layout-0.1.0.mk
@@ -3,13 +3,14 @@ P_VERSION		= 0.1.0
 P_DESCRIPTION	= Base roofs fs layout and configuration files
 P_LICENCES		= ASIS
 P_ARCHS			= arm
+P_SRCDIR		= layout
 
 .PHONY: fetch setup compile install
 
-# TODO: MAKE A REPOSITORY WITH THE FILES INSTEAD
-
 fetch:
-	@echo "nothing to fetch"
+	cookie import layout.tgz layout.tgz
+	tar xzf layout.tgz
+	rm layout.tgz
 
 setup:
 	@echo "nothing to setup"
@@ -18,31 +19,8 @@ compile:
 	@echo "nothing to compile"
 
 install:
-	mkdir -p $(P_DESTDIR)/dev
-	mkdir -p $(P_DESTDIR)/proc
-	mkdir -p $(P_DESTDIR)/root
-	mkdir -p $(P_DESTDIR)/run
-	mkdir -p $(P_DESTDIR)/sys
-	mkdir -p $(P_DESTDIR)/tmp
-	mkdir -p $(P_DESTDIR)/var
-	mkdir -p $(P_DESTDIR)/var/log
-	mkdir -p $(P_DESTDIR)/var/snes9x
-	mkdir -p $(P_DESTDIR)/media
-	mkdir -p $(P_DESTDIR)/usr
-	mkdir -p $(P_DESTDIR)/usr/bin
-	mkdir -p $(P_DESTDIR)/usr/sbin
-	mkdir -p $(P_DESTDIR)/etc
-	mkdir -p $(P_DESTDIR)/etc/init.d
-
-	cookie import fstab			$(P_DESTDIR)/etc/fstab
-	cookie import inittab		$(P_DESTDIR)/etc/inittab
-	cookie import rcS			$(P_DESTDIR)/etc/init.d/rcS
-	cookie import hostname		$(P_DESTDIR)/etc/hostname.conf
-	cookie import passwd		$(P_DESTDIR)/etc/passwd
-	cookie import group			$(P_DESTDIR)/etc/group
-	cookie import shells		$(P_DESTDIR)/etc/shells
-	cookie import fb.modes		$(P_DESTDIR)/etc/fb.modes
-	cookie import asound.conf	$(P_DESTDIR)/etc/asound.conf
-	cookie import mdev.conf		$(P_DESTDIR)/etc/mdev.conf
-	cookie import automount.sh	$(P_DESTDIR)/usr/bin/automount.sh
-	cookie import ifplugd_handler	$(P_DESTDIR)/usr/sbin/ifplugd_handler
+	mkdir -p $(P_DESTDIR)
+	cp -a . $(P_DESTDIR)/
+	mkdir -p $(P_DESTDIR)/boot
+	cookie import cmdline.txt $(P_DESTDIR)/boot/
+	cookie import config.txt $(P_DESTDIR)/boot/
