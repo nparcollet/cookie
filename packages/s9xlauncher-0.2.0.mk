ef8b4dce656aba12d8163314dbb65bcd818bcede
Nicolas Parcollet <parcollet@gmail.com>
2018-11-06 13:41:00 +0100

    profile/snes9x: updated to s9xlauncher-0.2.0

---
+++ packages/s9xlauncher-0.2.0.mk
@@ -0,0 +1,23 @@
+P_NAME			= s9xlauncher
+P_VERSION		= 0.2.0
+P_DESCRIPTION	= Snes Emulator
+P_GITURL		= https://github.com/nparcollet/s9xlauncher.git
+P_GITREV		= 6318b38e5403c66877374ae4bd1b97df62334bdd
+P_LICENCES		= MIT
+P_ARCHS			= arm
+P_DEPENDS		= sdl2 sdl2-ttf sdl2-image
+P_SRCDIR		= sources
+
+.PHONY: fetch setup compile install
+
+fetch:
+	cookie git clone $(P_GITURL) $(P_NAME)
+
+setup:
+	cookie git checkout $(P_NAME) $(P_GITREV) $(P_SRCDIR)
+
+compile:
+	make -j$(P_NPROCS)
+
+install:
+	make DESTDIR=$(P_DESTDIR) install
