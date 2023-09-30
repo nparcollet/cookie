ce9ed8871a008256042907c86973ad5fdbb99cd5
Nicolas Parcollet <parcollet@gmail.com>
2023-09-30 23:31:14 +0200

    Update kernel package

--- packages/kernel-6.1.54.mk
+++ packages/kernel-6.1.54.mk
@@ -9,7 +9,6 @@ P_ARCHS			= arm arm64
 P_SRCDIR		= linux-2ff65ffbdeb0c8764985af19df2a687a126136f4
 P_PROVIDES		= kernel
 P_DEPENDS		= firmware
-P_EXTRA_ENV		= P_BOARD_KERNEL P_BOARD_DEFCONFIG
 
 fetch:
 	cookie fetch $(P_URL) $(P_ARCHIVE)
@@ -17,24 +16,17 @@ fetch:
 setup:
 	cookie extract $(P_ARCHIVE) $(P_WORKDIR)
 
-# https://www.raspberrypi.com/documentation/computers/linux_kernel.html#default_configuration
-# NOTE: Kernel Headers are taken from the toolchain rootfs (sysroot) and thu not built/installed
+compile_config:
+	cookie import $(P_BOARD_DEFCONFIG).config .config || make ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- $(P_DEFCONFIG)_defconfig
 
-compile_headers:
-	make mrproper
-	KERNEL=kernel7 make ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- INSTALL_HDR_PATH=dest headers_install
-	find dest/include \( -name .install -o -name ..install.cmd \) -delete
-
-compile_arm:
-	cookie import $(P_BOARD_DEFCONFIG).config .config || KERNEL=$(P_BOARD_KERNEL) make ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- $(P_BOARD_DEFCONFIG)_defconfig
+compile_arm: compile_config
 	make -j$(NPROCS) PKG_CONFIG_SYSROOT_DIR="" ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- zImage modules dtbs
 
-compile_arm64:
-	cookie import $(P_BOARD_DEFCONFIG).config .config || KERNEL=$(P_BOARD_KERNEL) make ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- $(P_BOARD_DEFCONFIG)_defconfig
+compile_arm64: compile_config
 	make -j$(NPROCS) PKG_CONFIG_SYSROOT_DIR="" ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- Image modules dtbs
 
 compile: compile_$(ARCH)
-
+	
 install_arm:
 	make ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- INSTALL_MOD_PATH=$(P_DESTDIR)/ modules_install
 	mkdir -p $(P_DESTDIR)/boot/overlays
@@ -50,4 +42,6 @@ install_arm64:
 	cp arch/arm64/boot/dts/overlays/*.dtb* $(P_DESTDIR)/boot/overlays/
 
 install: install_$(ARCH)
-	find arch/$(ARCH)/ -name '*.dtb' | xargs -I{} cp {} $(P_DESTDIR)/boot/
+	make mrproper	
+	make ARCH=$(ARCH) CROSS_COMPILE=$(HOST)- INSTALL_HDR_PATH=$(P_DESTDIR)/usr headers_install
+	find $(P_DESTDIR)/usr/include \( -name .install -o -name ..install.cmd \) -delete
