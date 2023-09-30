23119ee43b8818f4c4508d39455c1d218ba1e43a
Nicolas Parcollet <parcollet@gmail.com>
2023-09-30 22:46:27 +0200

    Remove kernel headers from sysroot package

--- packages/sysroot-1.0.1.mk
+++ packages/sysroot-1.0.1.mk
@@ -1,5 +1,5 @@
 P_NAME			= sysroot
-P_VERSION		= 0.1.0
+P_VERSION		= 1.0.1
 P_DESCRIPTION	= Base sysroot imported from the target toolchain
 P_LICENCES		=
 P_ARCHS			= arm arm64
@@ -20,3 +20,4 @@ install:
 	cp -a /opt/target/toolchain/$(P_TOOLCHAIN)/sysroot/* $(P_DESTDIR)/
 	cp -a /opt/target/toolchain/$(P_TOOLCHAIN)/debug-root/usr/bin/*  $(P_DESTDIR)/usr/bin/
 	cp -a /opt/target/toolchain/$(P_TOOLCHAIN)/debug-root/usr/lib/*  $(P_DESTDIR)/usr/lib/
+	rm -rf $(P_DESTDIR)/usr/include/{asm,asm-generic,drm,linux,misc,mtd,rdma,scsi,sound,video,xen}
